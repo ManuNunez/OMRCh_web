2a57b641a973361176f48a5270b3fc058d73c31f
MNunez <manu.nunez.2004@gmail.com>
2024-01-24 07:51:12 -0600

    nav_btn added

---
+++ src/view/resources/js/nav_btn.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menuBtn').addEventListener('click', function() {
+      document.getElementById('menu').classList.toggle('hidden');
+    });
+  });
+  
