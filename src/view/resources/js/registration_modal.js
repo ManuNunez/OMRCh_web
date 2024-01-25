63e8f006226f2bc35afcba44dacdd4f01e3057d3
MNunez <manu.nunez.2004@gmail.com>
2024-01-24 23:52:11 -0600

    feat registration modal  added

---
+++ src/view/resources/js/registration_modal.js
@@ -0,0 +1,9 @@
+    // Función para abrir el modal
+    function openModal() {
+        document.getElementById('registrationModal').classList.remove('hidden');
+      }
+    
+      // Función para cerrar el modal
+      function closeModal() {
+        document.getElementById('registrationModal').classList.add('hidden');
+      }
