9bb2c4b0b7b77ac0523e3c679562cbd35d25d45d
MNunez <manu.nunez.2004@gmail.com>
2024-01-28 00:45:48 -0600

    feat : defailt content

---
+++ src/view/defaultContent.php
@@ -0,0 +1,6 @@
+<header class="bg-gray-800 text-white text-center py-20">
+		<div class="container mx-auto">
+			<h1 class="text-4xl font-bold mb-4">Desafiando Mentes, Transformando Futuros</h1>
+			<p class="text-lg">¡Bienvenido a la Olimpiada de Matemáticas de la Ribera de Chapala!</p>
+		</div>
+	</header>
