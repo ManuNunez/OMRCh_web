81a80269d2dd940c57df1b4b59b4cc78f1ec88b8
MNunez <manu.nunez.2004@gmail.com>
2024-01-28 01:36:40 -0600

    feat : login form section

---
+++ src/view/components/login.php
@@ -0,0 +1,9 @@
+<form class="bg-white p-8 rounded shadow-md w-96">
+    <label for="username" class="text-sm font-semibold text-gray-600">Nombre de Usuario:</label>
+    <input type="text" id="username" name="username" class="w-full py-2 px-3 border border-gray-300 rounded mt-1">
+
+    <label for="password" class="text-sm font-semibold text-gray-600 mt-4">Contraseña:</label>
+    <input type="password" id="password" name="password" class="w-full py-2 px-3 border border-gray-300 rounded mt-1">
+
+    <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded mt-4">Iniciar Sesión</button>
+</form>
