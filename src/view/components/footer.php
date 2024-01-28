8420bafe9c1c9a33eb4841c9b7d0fa7ed2e7e234
ikercfamoso <iker.covarrubias5286@alumnos.udg.mx>
2024-01-28 14:29:45 -0600

    Update footer/components

--- src/view/components/footer.php
+++ src/view/components/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-900 text-white py-4 fixed bottom-0 w-full">
+<footer class="bg-gray-900 text-white py-4 w-full">
     <div class="container mx-auto text-center">
         <p>&copy; 2024 Olimpiada de Matemáticas - Ribera de Chapala. Todos los derechos reservados.</p>
     </div>
