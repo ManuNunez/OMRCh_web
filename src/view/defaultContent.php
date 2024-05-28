09c6dc6f00e55c065e817fdb4221cc45637f6a4f
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-05-27 22:39:47 -0600

    this is a carrucel test

--- src/view/defaultContent.php
+++ src/view/defaultContent.php
@@ -1,6 +1,64 @@
-<header class="bg-gray-800 text-white text-center py-20">
-		<div class="container mx-auto">
-			<h1 class="text-4xl font-bold mb-4">Desafiando Mentes, Transformando Futuros</h1>
-			<p class="text-lg">¡Bienvenido a la Olimpiada de Matemáticas de la Ribera de Chapala!</p>
-		</div>
-</header>
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+
+</head>
+<body>
+
+
+
+
+<div class="w-full flex place-content-center ">
+    <div id="default-carousel" class="relative w-full h-96" data-carousel="slide">
+        <!-- Carousel wrapper -->
+        <div class="relative h-96 overflow-hidden   md:h-96">
+            <!-- Item 1 -->
+            <div class="hidden duration-2000 ease-in-out" data-carousel-item>
+                <img src="resources/imgs/carrusel/1.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 2 -->
+            <div class="hidden duration-2000 ease-in-out" data-carousel-item>
+                <img src="resources/imgs/carrusel/2.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 3 -->
+            <div class="hidden duration-2000 ease-in-out" data-carousel-item>
+                <img src="resources/imgs/carrusel/3.jpeg"  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 4 -->
+            <div class="hidden duration-2000 ease-in-out" data-carousel-item>
+                <img src="resources/imgs/carrusel/4.jpeg"  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 5 -->
+            <div class="hidden duration-2000 ease-in-out" data-carousel-item>
+                <img src="resources/imgs/carrusel/5.jpeg"  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+        </div>
+        <!-- Slider indicators -->
+        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+            <button type="button" class="w-3 h-3 rounded-full bg-blue-600 text-blue-700" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+            <button type="button" class="w-3 h-3 rounded-full bg-blue-600 text-blue-700" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+            <button type="button" class="w-3 h-3 rounded-full bg-blue-600 text-blue-700" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+            <button type="button" class="w-3 h-3 rounded-full bg-blue-600 text-blue-700" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+            <button type="button" class="w-3 h-3 rounded-full bg-blue-600 text-blue-700" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+        </div>
+        <!-- Slider controls -->
+        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-300  group-hover:bg-white/50  ">
+                <svg class="w-4 h-4 text-black  rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                </svg>
+                <span class="sr-only">anterior</span>
+            </span>
+        </button>
+        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-300  group-hover:bg-white/50  ">
+                <svg class="w-4 h-4 text-black rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg>
+                <span class="sr-only">siguiente</span>
+            </span>
+        </button>
+    </div>
+</div>
+
+</body>
