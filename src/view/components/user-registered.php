04006dc4eb753a27c0f78e9473a687713189a4a2
ikercfamoso <iker.covarrubias5286@alumnos.udg.mx>
2024-07-27 19:43:46 -0600

    some changes in page = user-registered

--- src/view/components/user-registered.php
+++ src/view/components/user-registered.php
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
 <header class="bg-gray-800 text-black text-center py-20" style="background-image: url(../public/images/IMG2.png); background-size: cover; background-position: left bottom;">
-=======
-<header class="bg-gray-800 text-black text-center py-20" style="background-image: url('../../public/images/IMG1.jpg'); background-size: cover; background-position: left bottom;">
->>>>>>> Stashed changes
+<header class="bg-white-800 text-black text-center py-20" style="background-image: url('../../public/images/IMG1.jpg'); background-size: cover; background-position: left bottom;">
 		<div style="width: 38%; margin-left:auto">
 			<h1 class="text-4xl font-bold mb-4">¡Registro compleado!</h1>
             <h3 class="text-2xl font-bold mb-4">Hemos enviado los detalles sobre la olimpiada a tu correo electrónico y al de tu profesor.</h3>
