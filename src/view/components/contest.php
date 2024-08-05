684e9e798c6a03a45ed787a7395a31e1e990c833
ikercfamoso <iker.covarrubias5286@alumnos.udg.mx>
2024-08-04 20:07:08 -0600

    feat: update form inscription

--- src/view/components/contest.php
+++ src/view/components/contest.php
@@ -42,7 +42,7 @@
                         <div class="text-sm text-gray-900"><?php echo htmlspecialchars($contest['contest_date'], ENT_QUOTES, 'UTF-8'); ?></div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-900"><?php echo htmlspecialchars($contest['duration_minutes'], ENT_QUOTES, 'UTF-8'); ?></div>
+                        <div class="text-sm text-gray-900"><?php echo htmlspecialchars($contest['duration_minutes'], ENT_QUOTES, 'UTF-8'); ?> minutos</div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
                         <button class="text-blue-500" onclick="openModal(<?php echo $contest['id']; ?>)">Inscribirme</button>
@@ -63,17 +63,41 @@
         <div class="bg-white p-8 rounded shadow-md w-1/2">
             <h2 id="modalTitle" class="text-2xl font-bold mb-4">Inscribirme</h2>
             <form id="inscripcionForm" method="POST">
+                
+                <label for="nombreProfesor" class="block text-sm font-medium text-gray-700 mt-4">Nombre de tu profesor:</label>
+                <input type="text" id="nombreProfesor" name="nombreProfesor" class="mt-1 p-2 border rounded-md w-full text-black" required>
+
+                
+                <label for="correoProfesor" class="block text-sm font-medium text-gray-700 mt-4">Correo de tu profesor:</label>
+                <input type="email" id="correoProfesor" name="correoProfesor" class="mt-1 p-2 border rounded-md w-full text-black" required>
+                
                 <label for="campusSelect" class="block text-sm font-medium text-gray-700">Selecciona un campus:</label>
                 <select id="campusSelect" name="campus" class="mt-1 p-2 border rounded-md w-full text-black">
                     <!-- Opciones dinámicas -->
                 </select>
+                
+                <!-- Select para nivel -->
+                <label for="nivelSelect" class="block text-sm font-medium text-gray-700 mt-4">Selecciona un nivel:</label>
+                <select id="nivelSelect" name="nivel" class="mt-1 p-2 border rounded-md w-full text-black">
+                    <option value="1">Nivel 1 (1 - 5 de Primaria)</option>
+                    <option value="2">Nivel 2 (6 de Primaria)</option>
+                    <option value="3">Nivel 3 (1 de Secundaria)</option>
+                    <option value="4">Nivel 4 (2 de Secundaria)</option>
+                    <option value="5">Nivel 5 (3 de Secundaria)</option>
+                    <option value="6">Nivel 6 (1 - 2 de Preparatoria)</option>
+                    <option value="7">Nivel 7 (3 - 4 de Preparatoria)</option>
+                    <option value="8">Nivel 8 (5 - 6 de Preparatoria)</option>
+                </select>
+
                 <div class="mt-4">
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Guardar</button>
-                    <button type="button" onclick="closeModal()" class="ml-2 text-gray-600">Cancelar</button>
+                    <button type="button" class="bg-blue-500 text-white px-4 py-2 rounded">Guardar</button>
+                    <button type="button" onclick="closeModal()" class="ml-2 text-gray-600">Regresar</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
+
 
 <script src="../controller/return_contest_campuses.js"></script>
