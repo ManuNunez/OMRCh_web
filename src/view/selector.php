b1c8244209c5d4f11d6a262a3184a5b39a47b2e6
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-08-07 19:42:35 -0600

    Update selector.php

--- src/view/selector.php
+++ src/view/selector.php
@@ -1,13 +1,13 @@
 <?php
 function contentSelector($section) {
-    $allowedSections = ['training', 'registration', 'login', 'user-registered','contest'];
+    $allowedSections = ['index','training', 'registration', 'login', 'user-registered','contest'];
     
     if (in_array($section, $allowedSections)) {
         $filename ='components/' . $section . '.php';
     } else {
         $filename = 'index.php';
     }
-    echo $filename;
+
 
     include_once $filename;
 }
