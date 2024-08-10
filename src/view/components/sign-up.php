439cfe14380c553361454587f21959c4ca243d32
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-08-10 14:35:58 -0600

    Create sign-up.php
    this is for the sign up form

---
+++ src/view/components/sign-up.php
@@ -0,0 +1,10 @@
+<head>
+    <link href="resources/main.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <script src="resources/js/jquery-3.3.1.min.js"></script>
+    <script src="resources/js/NavToggle.js"></script>
+</head>
+<body>
+
+    <p>sasa</p>
+</body>
