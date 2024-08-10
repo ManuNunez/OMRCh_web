9c830ce72d59e2bdb23db2517ffe0fc49ffd70eb
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-08-10 13:56:12 -0600

    Selector exception
    I added a verification in the selector so i wont show the nav and the footer in the section that don't need a nav or footer

--- src/view/index.php
+++ src/view/index.php
@@ -11,12 +11,23 @@
 <body class="font-sans bg-white flex flex-col min-h-screen">
 
 	<?php
+        $exception = ['section=sign-up']; // array de excepciones
+        $location = $_SERVER['QUERY_STRING']; //Obtengo el string de consulta de peticion de la pagina.
+        $flag = false; // Bandera para saber si el usuario esta en la pagina donde quiero la excepcion del nav y footer
+        foreach ($exception as $exc){
+            if($exc == $location){ // si existe entonces rompemos y 'flag' = true
+                $flag = true;
+                break;
+            }
+        }
+        if(!($flag)) // verificamos la bandera
 		include('components/nav.php');
 
 	?>
 
 
 	<?php
+        if(!($flag)) // verficamos la bandera
 		include('components/footer.php');
 	?>
 	<!-- <script src="resources/js/nav_btn.js"></script> -->
