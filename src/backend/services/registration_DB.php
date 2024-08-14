84b2e3b3d5cca7feda370dcc0dfa8e89d1577304
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-08-13 19:57:23 -0600

    Update registration_DB.php

--- src/backend/services/registration_DB.php
+++ src/backend/services/registration_DB.php
@@ -8,51 +8,6 @@ $student = array( //values requested
     "coach_name" => $_REQUEST['teacherName'],
     "coach_email" => $_REQUEST['teacherEmail']
 );
-function getStudentId($studentName,$conn){
-    try{
-        $query = "SELECT id From Students WHERE name = '$studentName'";
-        $res = $conn->query($query);
-        $row = $res->fetch_assoc();
-        return $row['id'];
-
-    }
-    catch (mysqli_sql_exception){
-        return json_encode(array("status"=>"0","error"=>"Error during the getStudentId function"));
-    }
-}
-function getSedeId($sedeinput,$conn){
-    $query ="SELECT id FROM Sedes WHERE status = 1 AND sede_name = '$sedeinput'";
-    $res = $conn->query($query);
-    if($res){
-        $row = $res->fetch_assoc();
-        return (int)$row['id'];
-    }else{
-        return null;
-    }
-
-}
-
-function getContestId($sede_id, $conn){
-    $query ="SELECT id FROM Contest WHERE status = 1 AND sede_id = '$sede_id'";
-    $res = $conn->query($query);
-    if($res){
-        $row = $res->fetch_assoc();
-        return $row['id'];
-    }else{
-        return null;
-    }
-
-}
-function insertStudents(array $student,$conn){
-    try{
-        $query =  "INSERT INTO Students (name, email, teacher_name, teacher_email) VALUES ('$student[username]', '$student[email]', '$student[coach_Name]', '$student[coach_Email]')";
-        $res = $conn->query($query);
-        return array("status"=>"1");
-    }
-    catch(mysqli_sql_exception){
-        return array("status"=>"0","error"=>"Error during the insertStudent function");
-    }
-}
 function createStudent(array $student, $conn) {
     try {
         // Validar y sanear entradas
@@ -104,23 +59,6 @@ function createStudent(array $student, $conn) {
 
 $res = createStudent($student, $conn);
 echo $res;
-// $student['sedeIdArray'] = $sede_id;// query for the id from the sede selected
-// $inStASW = insertStudents($student,$conn); // insert to the table Students
-// if($inStASW['status'] == "1"){
-//     $student['contestId'] = getContestId($student['sedeIdArray'],$conn);
-//     $student['studentId'] = getStudentId($student['username'],$conn);
-//     $inPaAsw = insertParticipants($student,$conn);
-//     $inPaDe = json_decode($inPaAsw);
-//     echo $inPaAsw;
-// }else{
-//     echo $inStASW;
-// }
-
-
-
 $conn = null;
-
-
-
 
 ?>
