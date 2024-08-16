323d5108f84a9d7f051b4aed0b268842f7d9fc2b
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-08-16 10:47:04 -0600

    Registro UX/UI
    getting done the UX/UI for the registration.php the next step is get done the end-point

--- src/view/components/sign-up.php
+++ src/view/components/sign-up.php
@@ -25,29 +25,42 @@
         <hr class="border-gray-300">
     </header>
 
-    <div class="my-auto">
-        <div class="container my-auto place-content-center  ">
-            <p class="font-extrabold text-4xl text-center ">Únete a la Competencia</p>
-        </div>
-<!--    <div class=" flex flex-cols-2 flex-end-2 container mx-auto">-->
+    <div class="my-auto mx-auto  ">
         <div class="inline-flex container my-auto place-content-center ">
-            <div class="w-1/4 ">
-                <div class="flex flex-col-2">
-                    <svg width="50px"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#3b82f6"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14 19.2857L15.8 21L20 17M4 21C4 17.134 7.13401 14 11 14C12.4872 14 13.8662 14.4638 15 15.2547M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z" stroke="#3b82f6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-                    <p class="m-5">Necesitas una cuenta para participar en la Olimpiada de Matemáticas.</p>
+
+            <div class="w-2/4 space-y-5">
+                <div class="container my-auto place-content-center  ">
+                    <p class="font-extrabold text-5xl text-center ">Únete a la Competencia</p>
                 </div>
                 <div class="flex flex-col-2">
-                    <svg width="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8.5 12.5L10.5 14.5L15.5 9.5" stroke="#3b82f6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#3b82f6" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
-                    <p class="m-5">Ten a la mano el CCT de tu escuela para registrarte.</p>
+                    <svg class="w-24 h-24 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
+                    </svg>
+                    <p class="my-6 mx-3">Necesitas una cuenta para participar en la Olimpiada de Matemáticas.</p>
                 </div>
+
                 <div class="flex flex-col-2">
-                    <svg fill="#3b80f6" width="50px"  viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <defs> <style> .cls-1 { fill: none; } </style> </defs> <path d="M21,2a8.9977,8.9977,0,0,0-8.6119,11.6118L2,24v6H8L18.3881,19.6118A9,9,0,1,0,21,2Zm0,16a7.0125,7.0125,0,0,1-2.0322-.3022L17.821,17.35l-.8472.8472-3.1811,3.1812L12.4141,20,11,21.4141l1.3787,1.3786-1.5859,1.586L9.4141,23,8,24.4141l1.3787,1.3786L7.1716,28H4V24.8284l9.8023-9.8023.8472-.8474-.3473-1.1467A7,7,0,1,1,21,18Z"></path> <circle cx="22" cy="10" r="2"></circle> <rect id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" class="cls-1" width="32" height="32"></rect> </g></svg>
-                    <p class="m-5">Protege tu cuenta con una contraseña única.</p>
+                    <svg class="w-16 h-16 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 9h3m-3 3h3m-3 3h3m-6 1c-.306-.613-.933-1-1.618-1H7.618c-.685 0-1.312.387-1.618 1M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Zm7 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                    </svg>
+                    <p class="my-1 mx-3">Ten a la mano el <a href="https://consultarcct.com/jalisco/" target="_blank" class="text-blue-500 hover:text-gray-400 hover:underline">CCT</a> de tu escuela para registrarte.</p>
+
                 </div>
+
+                <div class="flex-col-2 flex">
+                    <svg class="w-16 h-16 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.7" d="M9.5 11.5 11 13l4-3.5M12 20a16.405 16.405 0 0 1-5.092-5.804A16.694 16.694 0 0 1 5 6.666L12 4l7 2.667a16.695 16.695 0 0 1-1.908 7.529A16.406 16.406 0 0 1 12 20Z"/>
+                    </svg>
+                    <p class="my-5 mx-3">Protege tu cuenta con una contraseña única.</p>
+                </div>
+
                 <div class="flex flex-col-2">
-                    <svg width="50px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#3b80f6" stroke="#3b80f6"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>certificate-star</title> <g id="Layer_2" data-name="Layer 2"> <g id="invisible_box" data-name="invisible box"> <rect width="48" height="48" fill="none"></rect> <rect width="48" height="48" fill="none"></rect> </g> <g id="icons_Q2" data-name="icons Q2"> <path d="M40,18A16,16,0,1,0,15,31.2V43.9A2.1,2.1,0,0,0,17,46a1.5,1.5,0,0,0,1.1-.4L24,41l5.9,4.6A1.5,1.5,0,0,0,31,46a2.1,2.1,0,0,0,2-2.1V31.2A16,16,0,0,0,40,18ZM12,18A12,12,0,1,1,24,30,12,12,0,0,1,12,18ZM29,39.8l-4.4-3.4a.9.9,0,0,0-1.2,0L19,39.8V33.2a16.9,16.9,0,0,0,5,.8,16.9,16.9,0,0,0,5-.8Z"></path> <path d="M20.4,19.7l-.5,3.1a1.1,1.1,0,0,0,1.5,1.1L24,22l2.6,1.9a1.1,1.1,0,0,0,1.5-1.1l-.5-3.1,2.1-2a1.1,1.1,0,0,0-.6-1.8l-2.9-.4-1.3-2.9a1,1,0,0,0-1.8,0l-1.3,2.9-2.9.4a1.1,1.1,0,0,0-.6,1.8Z"></path> </g> </g> </g></svg>
-                    <p class="m-5">¡Sigue de cerca tus competencias!</p>
+                    <svg class="w-16 h-16 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m7.171 12.906-2.153 6.411 2.672-.89 1.568 2.34 1.825-5.183m5.73-2.678 2.154 6.411-2.673-.89-1.568 2.34-1.825-5.183M9.165 4.3c.58.068 1.153-.17 1.515-.628a1.681 1.681 0 0 1 2.64 0 1.68 1.68 0 0 0 1.515.628 1.681 1.681 0 0 1 1.866 1.866c-.068.58.17 1.154.628 1.516a1.681 1.681 0 0 1 0 2.639 1.682 1.682 0 0 0-.628 1.515 1.681 1.681 0 0 1-1.866 1.866 1.681 1.681 0 0 0-1.516.628 1.681 1.681 0 0 1-2.639 0 1.681 1.681 0 0 0-1.515-.628 1.681 1.681 0 0 1-1.867-1.866 1.681 1.681 0 0 0-.627-1.515 1.681 1.681 0 0 1 0-2.64c.458-.361.696-.935.627-1.515A1.681 1.681 0 0 1 9.165 4.3ZM14 9a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                    </svg>
+                    <p class="my-4 mx-3">¡Sigue de cerca tus competencias!</p>
                 </div>
+
             </div>
             <div class=" ">
                 <form class="w-64 ">
@@ -73,10 +86,18 @@
                             <label for="responsiveEmail" class="absolute text-sm text-gray-500 mx-1 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Correo Electrónico del Profesor/a</label>
                         </div>
 
-                        <div class="relative my-3 max-x-full">
+                        <div class="relative my-3 ">
                             <input type="password"  id="pass" name="pass" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                             <label for="pass" class="absolute text-sm text-gray-500 mx-1 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Contraseña</label>
                         </div>
+                        <div class="inline-flex my-3 relative w-full">
+                            <button  class="bg-blue-500 p-2 rounded hover:bg-blue-300 w-full">Registrarte</button>
+                        </div>
+                        <div class="m-1 relative ">
+                            <input type="checkbox" name="terms" id="terms">
+                            <label for="terms" class="text-gray-300 text-sm">acepto los terminos y condiciones de privacidad de mis datos.</label>
+                        </div>
+
                     </div>
                 </form>
             </div>
